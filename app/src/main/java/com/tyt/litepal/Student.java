6697c21d530e3ab6326a062de45ce1d70f258646
tyt <854580832@qq.com>
2017-11-01 17:19:05 +0800

    The First Commit

---
+++ app/src/main/java/com/tyt/litepal/Student.java
@@ -0,0 +1,54 @@
+package com.tyt.litepal;
+
+/**
+ * Created by Administrator on 2017-11-01.
+ */
+
+public class Student {
+    private int id;//可写可不写，自动有
+    private String name;
+    private String no;
+    private String sex;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", no='" + no + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
